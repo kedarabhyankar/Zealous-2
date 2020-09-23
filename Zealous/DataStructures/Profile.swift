fb710d8c1cae8203821928bdfe025a5658d27c31
Hannah Shiple <shiple@purdue.edu>
2020-09-23 19:31:17 -0400

    Small change to profile.swift and added code to profile settings backend

--- Zealous/DataStructures/Profile.swift
+++ Zealous/DataStructures/Profile.swift
@@ -10,20 +10,17 @@ import UIKit
 struct Profile {
     var username: String
     var email: String
-    var password: String
     var bio: String
     var picture: UIImageView
     
-    init(username: String, email: String, password: String, bio: String) {
+    init(username: String, email: String, bio: String) {
         self.username = username
         self.email = email
-        self.password = password
         self.bio = bio
         self.picture = UIImageView(image: UIImage(named: "default-profile-picture"))    }
-    init(username: String, email: String, password: String, bio: String, picture: UIImageView) {
+    init(username: String, email: String, bio: String, picture: UIImageView) {
         self.username = username
         self.email = email
-        self.password = password
         self.bio = bio
         self.picture = picture
     }
