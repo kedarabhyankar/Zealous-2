2a0e7cc1204f324c4d114939a728811c65687819
Hannah Shiple <shiple@purdue.edu>
2020-10-04 21:02:31 -0400

    can now upload pictures when creating a post

--- Zealous/CreatePostViewController.swift
+++ Zealous/CreatePostViewController.swift
@@ -14,23 +14,31 @@ import FirebaseFirestore
 import FirebaseStorage
 import FirebaseAuth
 import CodableFirebase
+import BRYXBanner
 
-class CreatePostViewController: UIViewController, UITextFieldDelegate {
+class CreatePostViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate & UINavigationControllerDelegate{
     
     
     @IBOutlet weak var PostTitle: UITextField!
     @IBOutlet weak var PostTopic: UITextField!
     @IBOutlet weak var PostCaption: UITextField!
     @IBOutlet weak var PostImage: UIImageView!
+    var imageExt: String?
+    var image: UIImage?
     var currentUser: WriteableUser? =  nil
     var currentPost: Post? = nil
     var currentTopic: Topic? = nil
+    var defaultImage: UIImage!
+    var finalProfile: Profile!
+    var imgURL: String = ""
     
     
     var db: Firestore!
     var storage: Storage!
     var userID = Auth.auth().currentUser!.uid
-    //var userID = UUID().uuidString
+    let unknownErrorBanner = Banner(title: "Something went wrong!", subtitle: "An unknown error occurred.", image: nil, backgroundColor: UIColor.red, didTapBlock: nil)
+    let successBanner = Banner(title: "Successfully uploaded your profile photo!", subtitle: "Your profile photo was successfully uploaded!", image: nil, backgroundColor: UIColor.green, didTapBlock: nil)    //var userID = UUID().uuidString
+    let bannerDisplayTime = 3.0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view
@@ -38,6 +46,7 @@ class CreatePostViewController: UIViewController, UITextFieldDelegate {
         Firestore.firestore().settings = firestoreSettings
         db = Firestore.firestore()
         storage = Storage.storage()
+        defaultImage = UIImage(systemName: "person.crop.circle")
         userID = Auth.auth().currentUser!.uid
         WriteableUser.getCurrentUser(completion: getUser)
     }
@@ -56,7 +65,7 @@ class CreatePostViewController: UIViewController, UITextFieldDelegate {
             let dataToWrite1 = try! FirestoreEncoder().encode(self.currentTopic)
             //find the topic in the database and update post array???
             self.db.collection("topics").document(self.currentTopic!.id).setData(dataToWrite1)
-        
+            
             //send current post to database
             let dataToWrite2 = try! FirestoreEncoder().encode(self.currentPost)
             self.db.collection("posts").document(self.currentPost!.postId).setData(dataToWrite2) {
@@ -76,14 +85,71 @@ class CreatePostViewController: UIViewController, UITextFieldDelegate {
             self.PostTitle.text = ""
             self.PostTopic.text = ""
             self.PostCaption.text = ""
+            self.PostImage.image = nil
             return
         }
-        // self.currentTopic = currentTopic
-        
     }
     
     
     @IBAction func UploadImage(_ sender: Any) {
+        //optional, user can attach an image to their post
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerController.SourceType.photoLibrary
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true, completion: nil)
+        PostImage.image = self.image
+        
+        uploadImg((self.image ?? self.defaultImage), completion: { (state, result) in
+            print("state \(state)")
+            print("result \(result)")
+            if (!state) {
+                self.unknownErrorBanner.show(duration: self.bannerDisplayTime)
+                return
+            } else {
+                self.imgURL = result
+                self.currentPost?.imgURL = result
+                return
+            }
+        })
+        return;
+    }
+    
+      func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+          var image: UIImage
+          if let possibleImage = info[.editedImage] as? UIImage{
+              image = possibleImage
+          } else if let possibleImage = info[.originalImage] as? UIImage {
+              image = possibleImage
+          } else {
+              return;
+          }
+          self.image = image
+          self.PostImage.image = image
+          self.imageExt = ".png"
+          self.dismiss(animated: true, completion: nil)
+      }
+    
+    
+    func uploadImg (_ image: UIImage, completion: @escaping (_ hasFinished: Bool,_ url: String) -> Void) {
+        let data: Data = image.jpegData(compressionQuality: 1.0)!
+        
+        let ref = Storage.storage().reference(withPath: "media/" + (self.currentUser?.email)! + "/" + "profile.jpeg")
+        ref.putData(data, metadata: nil,
+                    completion: { (meta, error) in
+                        if error == nil {
+                            //return url
+                            ref.downloadURL(completion: { (url, error) in
+                                if (error != nil) {
+                                    print ("some error happened here \(error!.localizedDescription)")
+                                    completion(false, "")
+                                } else {
+                                    completion(true, url!.absoluteString)
+                                }
+                            })
+                        }
+        })
     }
     
     @IBAction func SubmitPost(_ sender: Any) {
@@ -94,8 +160,7 @@ class CreatePostViewController: UIViewController, UITextFieldDelegate {
         let followerArray = [User]()
         let comments = [String]()
         let numLikes: Int = 0
-        let defaultImage: String = ""
-        
+        //let defaultImage: String = ""
         
         //if a field is left blank
         if (postTitle == "" || postTopic == "" || postCaption == "") {
@@ -106,11 +171,25 @@ class CreatePostViewController: UIViewController, UITextFieldDelegate {
             self.PostTitle.text = ""
             self.PostTopic.text = ""
             self.PostCaption.text = ""
+            self.PostImage.image = nil
+            return;
+        }
+        
+        if (postCaption.count > 500 || postTopic.contains(",")) {
+            //caption has to be less than 500 chars and only one topic per post
+            let alertController = UIAlertController(title: "Error", message: "Valid posts must be less than 500 characters and have only one topic.", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+            self.PostTitle.text = ""
+            self.PostTopic.text = ""
+            self.PostCaption.text = ""
+            self.PostImage.image = nil
             return;
         }
         
         //create a post object, have to add it to user's created post array and topic's post array
-        self.currentPost = Post.init(topic: postTopic, title: postTitle, caption: postCaption, creatorId: currentUser!.email, img: defaultImage)
+        self.currentPost = Post.init(topic: postTopic, title: postTitle, caption: postCaption, creatorId: currentUser!.email, img: imgURL)
         
         //need to check if a topic exists in the database, if it does not, need to create a new topic
         db.collection("topics").whereField("title", isEqualTo: postTopic).getDocuments() { (QuerySnapshot, err) in
@@ -147,45 +226,21 @@ class CreatePostViewController: UIViewController, UITextFieldDelegate {
                     }
                 }
                 //create post with the new topics
-                // self.db.collection("posts").document(postID).setData(["topic": postTopic, "postId" : postID, "creatorId" : Auth.auth().currentUser?.email! ?? self.userID, "title" : postTitle, "caption" : postCaption, "img" : postImage ?? "", "likes" : numLikes, "timeStamp" : FieldValue.serverTimestamp()])
                 self.PostTitle.text = ""
                 self.PostTopic.text = ""
                 self.PostCaption.text = ""
+                self.PostImage.image = nil
                 return
             }
             else {
                 //topic does exist, so add post to that topic's post array and add it to the database
                 print("Topic does exist")
                 //retrieve topic object and add current post to its post array
-                //Topic.getTopic(topicName: "CS", completion: self.getTheTopic)
                 DispatchQueue.main.async() {
-                    print("arg: " + postTopic)
                     Topic.getTopic(topicName: postTopic, completion: self.getTheTopic)
                 }
                 print("current topic in submit post: \(String(describing: self.currentTopic))")
                 
-                //add post to topic's array
-                //self.currentTopic?.addPost(post: self.currentPost!)
-                //encode the updated post array
-                //let dataToWrite1 = try! FirestoreEncoder().encode(self.currentTopic)
-                //find the topic in the database and update post array???
-                //self.db.collection("topics").document(self.currentTopic!.id).setData(dataToWrite1)
-                
-                //send current post to database
-                //let dataToWrite2 = try! FirestoreEncoder().encode(self.currentPost)
-               /* self.db.collection("posts").document(self.currentPost!.postId).setData(dataToWrite2) {
-                    error in
-                    if (error != nil) {
-                        print("error writing post to firestore: \(String(describing: error))")
-                        return
-                    } else {
-                        print("success writing post to firestore")
-                    }
-                }
-                self.PostTitle.text = ""
-                self.PostTopic.text = ""
-                self.PostCaption.text = ""*/
-            
                 return
                 
             }
@@ -199,6 +254,6 @@ class CreatePostViewController: UIViewController, UITextFieldDelegate {
     
     
     
-
-
+    
+    
 }
