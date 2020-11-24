428107336e5870a941af6cfaa650ce33db9e4242
Hannah Shiple <shiple@purdue.edu>
2020-11-24 16:55:45 -0500

    admin delete topic done

--- Zealous/AdminTopicsViewCell.swift
+++ Zealous/AdminTopicsViewCell.swift
@@ -20,6 +20,24 @@ class AdminTopicsViewCell: UITableViewCell {
     }
 
     @IBAction func deleteClicked(_ sender: Any) {
+        var topicName = topic.text
+        Topic.getTopic(topicTitle: topicName!, completion: getTopic)
+    }
+    
+    func getTopic(theTopic: Topic) {
+        var myTopic = theTopic
+        let serialQ = DispatchQueue(label: "com.queue.serial")
+        serialQ.sync {
+        for aPost in theTopic.posts {
+            myTopic.removePost(postId: aPost)
+            //Post.deleteStoragePost(thePost: aPost, theUser: self.currentUser!)
+            Post.deletePost(postId: aPost)
+            
+        }
+        }
+        
+        Topic.deleteTopic(topicName: theTopic.title)
+        
     }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
